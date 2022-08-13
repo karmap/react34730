9a8d628373ce0700e98c099fffcfce67a3dc38c4
Adrian Escalante <adresc@gmail.com>
2022-08-13 13:51:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>¡Hola Mundo!</h1>
+      <h3>Otro texto</h3>
+      <li>dfsdf</li>
+      <li>dfsdf</li>
+      <li>dfsdf</li>
+      <li>dfsdf</li>
+      <li>dfsdf</li>
+      <li>dfsdf</li>
+    </>
+  );
+}
+
+export default App;
