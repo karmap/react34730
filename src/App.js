411c9031e3cd149fc64a11e7c8589af4e97bb81c
Adrian Escalante <adresc@gmail.com>
2022-08-20 11:21:47 -0500

    clase 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const mensaje = 'Bienvenido a mi app'
+
   return (
-    <>
+    <div className='container'>
       <h1>¡Hola Mundo!</h1>
-      <h3>Otro texto</h3>
-      <li>dfsdf</li>
-      <li>dfsdf</li>
-      <li>dfsdf</li>
-      <li>dfsdf</li>
-      <li>dfsdf</li>
-      <li>dfsdf</li>
-    </>
+      <strong>Otro mensaje</strong>
+      <h3 style={ {backgroundColor: '#888'} }>{ mensaje }</h3>
+    </div>
   );
 }
 
