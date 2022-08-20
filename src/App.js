1992fd0a957838492de083cb0e90110fbcb8908f
Adrian Escalante <adresc@gmail.com>
2022-08-20 13:00:47 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import Saludo from './components/Saludo';
 
-function App() {
+function App(props) {
 
   const mensaje = 'Bienvenido a mi app'
+  const estilos = {
+    backgroundColor: '#888',
+    padding: '20px'
+  }
 
   return (
     <div className='container'>
       <h1>¡Hola Mundo!</h1>
       <strong>Otro mensaje</strong>
-      <h3 style={ {backgroundColor: '#888'} }>{ mensaje }</h3>
+      <h3 style={ estilos }>{ mensaje }</h3>
+      <Saludo name="Juan" lastname='Pérez'/>
+      <Saludo name='Ana' lastname='López'/>
+      <Saludo name='Miguel' lastname='Gonzalez'/>
     </div>
   );
 }
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,12 @@
+const Saludo = ( {name, lastname} ) => {
+  return (
+    <h1>¡Saludos {name} {lastname}!</h1>
+  )
+}
+export default Saludo
+
+// export default function Saludo() {
+//   return (
+//     <div>Saludo</div>
+//   )
+// }
